802df1a4c6f925ad2f29901f9d6132c5e4c2be60
QPAvlo <pav.andriiv650@gmail.com>
2023-03-08 17:58:22 +0200

    epoll + keep alive

--- epoll/main.c
+++ epoll/main.c
@@ -7,46 +7,31 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/epoll.h>
+#include <pthread.h>
 
 #define MAX_EVENTS 10
 #define PORT 5000
+#define MAX_THREADS 8
 
-int main() {
-    int server_socket, new_socket, epoll_fd;
-    struct sockaddr_in address;
+struct thread_args {
+    int serv_sock;
+};
+
+void *thread_foo(void *args) {
+
+    struct thread_args t_args = *(struct thread_args *) args;
+
+    int server_socket = t_args.serv_sock;
+
+    int new_socket;
+    int epoll_fd;
     struct epoll_event event, events[MAX_EVENTS];
-
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    int opt = 1;
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("Setsockopt failed");
-        exit(EXIT_FAILURE);
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    if (bind(server_socket, (struct sockaddr *) &address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_socket, 3) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
 
     if ((epoll_fd = epoll_create1(0)) < 0) {
         perror("Epoll creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Add server socket to epoll
     event.events = EPOLLIN;
     event.data.fd = server_socket;
 
@@ -92,6 +77,7 @@ int main() {
                                       "Content-Type: text/html\n"
                                       "Connection: Closed\n"
                                       "\n"
+                                      "<!DOCTYPE html>"
                                       "<html>\n"
                                       "<body>\n"
                                       "<h1>Hello, World!</h1>\n"
@@ -111,17 +97,74 @@ int main() {
                         exit(EXIT_FAILURE);
                     }
 
-                    close(events[i].data.fd);
+//                    close(events[i].data.fd);
                 } else {
                     // Print received data and echo it back to client
 //                    buffer[nbytes] = '\0';
                     printf("Received data: %s\n", buffer);
                     write(events[i].data.fd, buffer1, strlen(buffer1));
-                    close(events[i].data.fd);
+//                    close(events[i].data.fd);
+                }
+
+                int keep_alive = 0;
+                socklen_t len = sizeof(keep_alive);
+                if (getsockopt(new_socket, SOL_SOCKET, SO_KEEPALIVE, &keep_alive, &len) < 0) {
+                    perror("getsockopt failed");
+                    exit(EXIT_FAILURE);
+                }
+                if (!keep_alive) {
+                    close(new_socket);
                 }
             }
         }
     }
+}
+
+int main() {
+    int server_socket;
+    struct sockaddr_in address;
+//    struct epoll_event event, events[MAX_EVENTS];
+
+    pthread_t threads[MAX_THREADS];
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int opt = 1;
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT | SO_KEEPALIVE, &opt, sizeof(opt))) {
+        perror("Setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_socket, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Add server socket to epoll
+    struct thread_args args;
+    args.serv_sock = server_socket;
+
+    for (int i = 0; i < MAX_THREADS; ++i) {
+        pthread_create(&threads[i], NULL, thread_foo, &args);
+    }
+
+    for (int i = 0; i < MAX_THREADS; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
+    close(server_socket);
 
     return 0;
 
